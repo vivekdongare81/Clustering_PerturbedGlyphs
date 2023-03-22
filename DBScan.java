df211e2d30bbe04b7ad16d79c48311f5396dea3e
Vivek Dongare <dongarevivek81@gmail.com>
2023-03-22 20:56:41 +0530

    Update DBScan.java

--- DBScan.java
+++ DBScan.java
@@ -8,7 +8,7 @@ public class DBScan {
     public static void mainDBScan(double[][]dataset) {
      
         double eps = 20;
-        int minPts = 5;
+        int minPts = 3;
        
         // Run DBSCAN algorithm
         ArrayList<ArrayList<Double>> clusters = dbscan(dataset, eps, minPts);
@@ -37,17 +37,17 @@ public class DBScan {
         	for(int j=0;j<SVGtoVector.xyCoordinates.get(i).size();j++) {
         		total++;
              if(hm.containsKey(SVGtoVector.xyCoordinates.get(i).get(j))){
-            	 //System.out.println();
+            	// System.out.println();
         		   if(Double.compare(hm.get(SVGtoVector.xyCoordinates.get(i).get(j)),SVGtoVector.xyCoordinates.get(i).get(j+1)) == 0) {
         			   
         			   cnt++;
         			   }	
         		}else {
- 				   //System.out.println();
+ 				  // System.out.println();
  			   }
              j++;
         	}
-        	System.out.println("for File- "+(i+1)+" total co- "+total+" incluster co- "+cnt);
+        	System.out.println("for File- "+(i+1)+" "+SVGtoVector.fileNames.get(i)+" total co- "+total+" incluster co- "+cnt);
         }
         
     }
